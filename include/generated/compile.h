87f57582c63a0f88a1fdb5197c8f5a6c51c70dcf
John Thatcher <john_thatcher@ymail.com>
2013-02-16 22:16:34 -0500

    Updated to snappy 71

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 5 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP PREEMPT Sat Feb 16 19:45:58 EST 2013"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "thatch-xps"
-#define LINUX_COMPILER "gcc version 4.7.3 20121106 (prerelease) (Linaro GCC 4.7-2012.11) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.1.065Siyah-2.03-JT"
