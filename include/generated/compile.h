52fc1c45547af63a042a195af2fd7590bcc4a124
John Thatcher <john_thatcher@ymail.com>
2013-02-25 08:42:19 -0500

    Merge new CM10.1 Mali Driver

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 18 */
+/* This file is auto generated, version 19 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#18 SMP PREEMPT Sun Feb 24 21:25:37 EST 2013"
+#define UTS_VERSION "#19 SMP PREEMPT Mon Feb 25 08:32:27 EST 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "thatch-xps"
 #define LINUX_COMPILER "gcc version 4.7.3 20130102 (prerelease) (Linaro GCC 4.7-2013.01) "
