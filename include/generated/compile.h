49679085b025f8f0dac46b1823518394fcd67915
John Thatcher <john_thatcher@ymail.com>
2013-03-04 08:34:12 -0500

    revert BS

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 46 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#46 SMP PREEMPT Fri Mar 1 15:02:51 EST 2013"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Mar 4 08:28:52 EST 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "thatch-xps"
 #define LINUX_COMPILER "gcc version 4.7.3 20130102 (prerelease) (Linaro GCC 4.7-2013.01) "
