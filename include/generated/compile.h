f5a10196fdd79d54612dbe5b7c874e811d345f97
John Thatcher <john_thatcher@ymail.com>
2013-04-02 23:16:52 -0400

    Kernel 2.36!

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 SMP PREEMPT Tue Apr 2 18:39:25 EDT 2013"
+#define UTS_VERSION "#12 SMP PREEMPT Tue Apr 2 23:04:56 EDT 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "thatch-xps"
 #define LINUX_COMPILER "gcc version 4.7.3 20130205 (prerelease) (Linaro GCC 4.7-2013.02-01) "
