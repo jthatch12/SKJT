f75a5c0bbb8140cd1cedb70c95ee2bcb63229159
John Thatcher <john_thatcher@ymail.com>
2013-04-02 23:31:35 -0400

    Latest 3.37EX1

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 13 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#12 SMP PREEMPT Tue Apr 2 23:04:56 EDT 2013"
+#define UTS_VERSION "#13 SMP PREEMPT Tue Apr 2 23:31:20 EDT 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "thatch-xps"
 #define LINUX_COMPILER "gcc version 4.7.3 20130205 (prerelease) (Linaro GCC 4.7-2013.02-01) "
