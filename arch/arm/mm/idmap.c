cb85912b401b9820e27a862f7e8b7c5b93718987
John Thatcher <john_thatcher@ymail.com>
2013-04-12 11:34:10 -0400

    Update to latest Linaro 13.03

--- arch/arm/mm/idmap.c
+++ arch/arm/mm/idmap.c
@@ -65,6 +65,8 @@ static int __init init_static_idmap(void)
 		(long long)idmap_start, (long long)idmap_end);
 	identity_mapping_add(idmap_pgd, idmap_start, idmap_end);
 
+
+
 	return 0;
 }
 early_initcall(init_static_idmap);
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 24 */
+/* This file is auto generated, version 29 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#24 SMP PREEMPT Wed Apr 3 16:44:06 EDT 2013"
+#define UTS_VERSION "#29 SMP PREEMPT Tue Apr 9 22:46:54 EDT 2013"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "thatch-xps"
-#define LINUX_COMPILER "gcc version 4.7.3 20130205 (prerelease) (Linaro GCC 4.7-2013.02-01) "
+#define LINUX_COMPILER "gcc version 4.7.3 20130226 (prerelease) (Linaro GCC 4.7-2013.03) "
